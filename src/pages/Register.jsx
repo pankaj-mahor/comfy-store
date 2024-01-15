472ad0b78f50592e86d1d3a5f2464b514bd0df70
pankaj-mahor <mahorpankaj12@gmail.com>
2024-01-15 14:37:39 +0530

    ui signup page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Register = () => {
-	return <div>Register</div>;
+	return (
+		<div className="signup-page card">
+			<h1>Register</h1>
+		</div>
+	);
 };
 
 export default Register;
