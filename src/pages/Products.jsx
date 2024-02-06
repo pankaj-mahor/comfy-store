841fd438612b6c9030ce34abd9506650777fc8d6
pankaj-mahor <mahorpankaj12@gmail.com>
2024-02-06 22:06:02 +0530

    feat: Products listing with grid and list filter

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Filters, PaginationContainer, ProductsContainer } from "../components";
+import { customFetch } from "../utils";
+const url = "/products";
+
+export const loader = async ({ request }) => {
+	const response = await customFetch(url);
+	// console.log(response);
+
+	const products = response.data.data;
+	const meta = response.data.meta;
+	return { products, meta };
+};
 
 const Products = () => {
-	return <div>Products</div>;
+	return (
+		<div>
+			<Filters />
+			<ProductsContainer />
+			<PaginationContainer />
+		</div>
+	);
 };
 
 export default Products;
